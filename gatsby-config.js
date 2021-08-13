7febea03f0dc12585e1c5d888c21993be3b8e09c
Yulianny Betancourt <flipsdulce@gmail.com>
2021-08-13 16:42:06 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "website-official",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
