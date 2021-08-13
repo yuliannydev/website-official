bb0092d17caf4abb51c07c0561cd5ea957137d2b
Yulianny Betancourt <flipsdulce@gmail.com>
2021-08-13 17:47:18 -0400

    :tada: Begin a project
    Init project to my website with gatsby

---
+++ src/styles/styles.js
@@ -0,0 +1,27 @@
+/* STYLES */
+export const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+
+export const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+
+export const headingAccentStyles = {
+  color: "#663399",
+};
+
+export const paragraphStyles = {
+  marginBottom: 48,
+};
+
+/* export const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+}; */
