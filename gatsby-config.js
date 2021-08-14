4642bff8900e62153abd77bb8414e646f6479333
Yulianny Betancourt <flipsdulce@gmail.com>
2021-08-14 15:10:20 -0400

    :sparkles: Introduce new features
    Add the Headless CMS - GraphCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,19 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.yuliannydev.me",
     title: "website-official",
   },
   plugins: [
     "gatsby-plugin-emotion",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-graphcms",
+      options: {
+        endpoint: process.env.GRAPHCMS_ENDPOINT,
+        token: process.env.GRAPHCMS_TOKEN,
+      },
+    },
   ],
 };
