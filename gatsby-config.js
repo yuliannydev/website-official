d776665f9f370677e2aaeedeafffa17344955be6
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-28 21:33:14 -0400

    :mag: Improve SEO.
    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     twitterUsername: "@yuliannydev",
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
