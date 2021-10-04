86267d96697b2fe8995b1c64053d6548c450f2c6
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-03 20:38:14 -0400

    :heavy_plus_sign: Add dependency to ChackraUI

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
-    "gatsby-plugin-emotion",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-react-helmet",
     "@chakra-ui/gatsby-plugin",
